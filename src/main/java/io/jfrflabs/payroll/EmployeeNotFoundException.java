f9872d98b5ac96b58c4f7b6e8840dc12369f0161
José Fernando Falcao <jfribeirofalcao@gmail.com>
2019-11-09 17:25:55 -0500

    Added Employee Controller and Error management classes

---
+++ src/main/java/io/jfrflabs/payroll/EmployeeNotFoundException.java
@@ -0,0 +1,7 @@
+package io.jfrflabs.payroll;
+
+public class EmployeeNotFoundException extends RuntimeException {
+    public EmployeeNotFoundException(Long id) {
+        super("Could not find employee " + id);
+    }
+}
